d1949f45baa404bef2ed99f4820427c0d7f86235
Vaneshree <vaneshreenair@gmail.com>
2023-04-27 07:07:09 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
 * main - Test function for positive or negative
 * Return: 0
@@ -12,5 +11,7 @@ int main(void)
 	int i;
 	i = 0;
 	positive_or_negative(i);
-	return (0);
+
+return (0);
 }
+
